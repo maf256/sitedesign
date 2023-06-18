ce4068c2cc026ff5c2dd35e5935ffd0f8fab27ef
maf256 <maf256@gmail.com>
2023-06-18 23:23:15 +0200

    added Head in Home

--- components/home/Home.js
+++ components/home/Home.js
@@ -2,36 +2,48 @@ import styled from "styled-components"
 import Image from 'next/image'
 import Img from '../common/image/me.png'
 import {Phone, Email, FaceBook, Twitter, Linkedin, Github, Whatsapp, Business} from '../common/Svg'
+import Head from 'next/head';
+
 
 
 
 export default function Home() {
   return (
-    <Main >
-        <div>
-            <Image
-                src = {Img}
-                width = {250}
-                height = {250}
-                alt = "Majid Askarifarsangi"
-                placeholder="blur"
-                priority={false}
-                loading = 'lazy'
-            />
-        </div>
-        <div>
-            <H1>Majid Askarifarsangi</H1>
-            <p>I am a web developer</p>
-            <p>I build things for the web.</p>
-            <SosialConteiner>
-              <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/farsangi'><Linkedin fontSize="20px" margin="18px"/></a>
-              <a target="_blank" rel="noreferrer" href='https://github.com/maf256'><Github fontSize="20px" margin="18px"/></a>
-              <a target="_blank" rel="noreferrer"  href="tel:+4740882869"> <Phone fontSize="20px" margin="0 15px 0 0"/></a>
-              <a target="_blank" rel="noreferrer"  href="https://wa.me/+4740882869"><Whatsapp fontSize="20px" margin="0 15px 0 0"/></a>
-              <a target="_blank" rel="noreferrer"  href="mailto:info@sitedesign.no"><Email fontSize="20px" margin="0 15px 0 0"/></a>
-            </SosialConteiner>
-        </div>
-    </Main>
+    <>
+      <Head>
+        <title>Webutvikler</title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
+        />
+      </Head>
+      <Main >
+          <div>
+              <Image
+                  src = {Img}
+                  width = {250}
+                  height = {250}
+                  alt = "Majid Askarifarsangi"
+                  placeholder="blur"
+                  priority={false}
+                  loading = 'lazy'
+              />
+          </div>
+          <div>
+              <H1>Majid Askarifarsangi</H1>
+              <p>I am a web developer</p>
+              <p>I build things for the web.</p>
+              <SosialConteiner>
+                <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/farsangi'><Linkedin fontSize="20px" margin="18px"/></a>
+                <a target="_blank" rel="noreferrer" href='https://github.com/maf256'><Github fontSize="20px" margin="18px"/></a>
+                <a target="_blank" rel="noreferrer"  href="tel:+4740882869"> <Phone fontSize="20px" margin="0 15px 0 0"/></a>
+                <a target="_blank" rel="noreferrer"  href="https://wa.me/+4740882869"><Whatsapp fontSize="20px" margin="0 15px 0 0"/></a>
+                <a target="_blank" rel="noreferrer"  href="mailto:info@sitedesign.no"><Email fontSize="20px" margin="0 15px 0 0"/></a>
+              </SosialConteiner>
+          </div>
+      </Main>
+    </>
   )
 }
 
