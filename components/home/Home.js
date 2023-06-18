e1bf7fb536e900a1e386827d1680600792662101
maf256 <maf256@gmail.com>
2023-06-18 03:27:52 +0200

    added loading = lazy in image

--- components/home/Home.js
+++ components/home/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
                 alt = "Majid Askarifarsangi"
                 placeholder="blur"
                 priority={false}
-                loading = 'eager'
+                loading = 'lazy'
             />
         </div>
             <H1>Majid Askarifarsangi</H1>
