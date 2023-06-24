508e4b71899004084d4b27b692d67f444adae2c8
maf256 <maf256@gmail.com>
2023-06-25 00:32:46 +0200

    Added meta tag and removed extera menu item

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,18 @@ export default function Homepage ({language}){
         <   title>Webutvikler</title>
             <meta
             name="description"
-            content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+            content="web utvikler - Webdesign sandvika- webdesign oslo - webutvikler - react - motion designer - web nod - webdesigns templates - Responsive- Responsiveness - wordpress - woocommerce - Responsive web design - Responsive page - Responsive webdesign - webdesign, Webshop, Programming, Wordpress, SEO, Digital marketing, Graphic design.
+                webdesign webdesign design webdesign development -  webshop site web -  it consulting - programming wordpress graphic design sandvika oslo Norway bærum digitalm arketing - nettside nettside design nettside utvikling -  nettshop -  it konsulent - utvikling grafisk design digital markedsføring social marketing sosial markeds føring facebook Instagram - linkedin - SEO - CRO"
             key="desc"
             />
+            <meta 
+                name="keywords" content="HTML,CSS,XML,JavaScript,react,web development, responsive design, front-end development, back-end development,"
+            />
+            <meta name="author" content="Majid Askarifarsangi"/>
+            <meta  name="robots" content=" index , follow "/>
         </Head>
         <Home language={language}/>
     </>
 }
+
+
