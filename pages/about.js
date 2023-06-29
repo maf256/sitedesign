282005c190faa9c62e7c3dd428db6ed8598b9ae6
maf256 <maf256@gmail.com>
2023-06-29 19:23:28 +0200

    update meta tag

--- pages/about.js
+++ pages/about.js
@@ -9,9 +9,9 @@ export default function about ({language}){
         <   title>Webutvikler</title>
             <meta
             name="description"
-            content="web utvikler - Webdesign sandvika- webdesign oslo - webutvikler - react - motion designer - web nod - webdesigns templates - Responsive- Responsiveness - wordpress - woocommerce - Responsive web design - Responsive page - Responsive webdesign - webdesign, Webshop, Programming, Wordpress, SEO, Digital marketing, Graphic design.
+            content="web utvikler - Webdesign sandvika - webdesign oslo - webutvikler - react - motion designer - Responsive- Responsiveness - wordpress - woocommerce - Responsive web design - Responsive page - Responsive webdesign - webdesign, Webshop, Programming, Wordpress, SEO, Digital marketing, Graphic design.
                 webdesign webdesign design webdesign development -  webshop site web -  it consulting - programming wordpress graphic design sandvika oslo Norway bærum digitalm arketing - nettside nettside design nettside utvikling -  nettshop -  it konsulent - utvikling grafisk design digital markedsføring social marketing sosial markeds føring facebook Instagram - linkedin - SEO - CRO"
-            key="desc"
+           key="desc"
             />
             <meta 
                 name="keywords" content="webutvikler, HTML ,CSS ,XML ,JavaScript ,react ,web development, responsive design, front-end development, back-end development,"
