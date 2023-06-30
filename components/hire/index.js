37de0dc333e0bf068eb271ef978fd644b0ce2ddc
maf256 <maf256@gmail.com>
2023-06-30 23:56:26 +0200

    update Item

--- components/hire/index.js
+++ components/hire/index.js
@@ -18,7 +18,7 @@ export default function Hire ({language}) {
                         }
                     ]
                 })
-            })}>Buy</button>
+            })}>Buy Items</button>
         </ContactContainer>
 
     )
