e16c12249f195831669bf8583033118265892939
maf256 <maf256@gmail.com>
2023-07-02 20:42:54 +0200

    working on Booking

--- components/booking/GoogleCalendarScheduling.js
+++ components/booking/GoogleCalendarScheduling.js
@@ -5,24 +5,25 @@ const GoogleCalendarScheduling = ({language}) => {
   useEffect(() => {
     const loadCalendar = () => {
       calendar.schedulingButton.load({
-        url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0gcKmY01opz5BqfU2mLkHVN2UtcqyqfMQlH13JAEdUsluHCSD_eg-H3wtUYosT4y5VF8-IBO8d?gv=true',
+        url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ08q78jfdTSAvdjzrYsZzvFRPIxbVtTln79uu3M9ITm3VITP81IoKX-YurFrpjs3PoE-IG9z8je?gv=true',
         color: '#039BE5',
         label: Data.booking[language ? 'en' : 'nr'].textlabel,
         // label: 'Book an appointment',
         target: document.getElementById('calendar-scheduling-button'),
       });
     };
-
-    if (typeof calendar === 'undefined') {
+    if (typeof calendar === 'undefined' ) {
       const script = document.createElement('script');
       script.src = 'https://calendar.google.com/calendar/scheduling-button-script.js';
       script.async = true;
       script.onload = loadCalendar;
       document.body.appendChild(script);
+
     } else {
-      loadCalendar();
+
+        loadCalendar();
     }
-  }, [language]);
+  }, []);
 
   return (
     <div id="calendar-scheduling-button">
@@ -31,3 +32,23 @@ const GoogleCalendarScheduling = ({language}) => {
 };
 
 export default GoogleCalendarScheduling;
+
+
+
+// <!-- Google Calendar Appointment Scheduling begin -->
+// <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+// <script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
+// <script>
+// (function() {
+//   var target = document.currentScript;
+//   window.addEventListener('load', function() {
+//     calendar.schedulingButton.load({
+//       url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ08q78jfdTSAvdjzrYsZzvFRPIxbVtTln79uu3M9ITm3VITP81IoKX-YurFrpjs3PoE-IG9z8je?gv=true',
+//       color: '#039BE5',
+//       label: 'Book an appointment',
+//       target,
+//     });
+//   });
+// })();
+// </script>
+// <!-- end Google Calendar Appointment Scheduling --></link>
