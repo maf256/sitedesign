660c583f4f9ad05e80cef81fd6af636057407e30
maf256 <maf256@gmail.com>
2023-07-10 21:03:19 +0200

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import { AboutPage } from '../components/about/pageStyled'
+
+export default function Custom404() {
+  return <AboutPage>
+        <h1>404 - Page Not Found</h1>
+    </AboutPage>
+}
+
+
+
