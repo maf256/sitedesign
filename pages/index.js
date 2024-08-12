b292664b738a523891b8b5f6e318145ac25fd0f8
Majid Askarifarsangi <Majid-2@celesteai.com>
2024-08-12 11:06:18 +0200

    Changed index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
 import Home from "../components/home/Home";
 import Head from 'next/head';
-// import Icon from './favicon.png'
-import Img from '../components/common/image/me.png'
+import Icon from '../components/common/image/favicon.png'
+import Img from '../components/common/image/me.jpg'
 
 
 export default function Homepage ({language}){
-    const structuredData = {
-        "@context": "http://schema.org",
-        "@type": "Person",
-        "name": "Majid Askarifarsangi",
-        "image": "../components/common/image/me.png",
-        "url": "https://sitedesign.no",
-        "sameAs": [
-          "https://www.facebook.com/majid.farsangi",
-          "https://www.linkedin.com/in/farsangi/"
-        ]
-      };
 
     return <>
         <Head>
@@ -26,15 +15,18 @@ export default function Homepage ({language}){
             key="desc"
             />
             <meta 
-                name="keywords" content="Webutvikler, Webutvikling, JavaScript,react,web development, responsive design, front-end development, back-end development"
+                name="keywords" content="Webutvikler,Webutvikling,CSS,JavaScript,react,web development, responsive design, front-end development, back-end development"
             />
             <meta name="author" content="Majid Askarifarsangi"/>
             <meta  name="robots" content=" index , follow "/>
+            <link rel="icon" type="image/png" sizes="16x16" href={Icon}/>
             <link rel="canonical" href="https://sitedesign.no" />
 
             <meta property="og:title" content="Webutvikler webutvikling webdesign & web utvikler i oslo" />
             <meta property="og:description" content="Webutvikler webutvikling webdesign & web utvikler i oslo" />
             <meta property="og:image" content={Img} />
+            <link rel="icon" type="image/x-icon" href={Icon} />
+
             <meta property="og:url" content="https://sitedesign.no" />
             {/* Optional tags */}
             <meta property="og:site_name" content="sitedeisgn" />
@@ -42,20 +34,6 @@ export default function Homepage ({language}){
 
             {/* LinkedIn link */}
             <meta property="og:see_also" content="https://www.linkedin.com/in/farsangi/" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            {/* ___________________________________ */}
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-            <meta name="msapplication-TileColor" content="#da532c" />
-            <meta name="theme-color" content="#ffffff"></meta>
-            {/* _____________________ */}
-            <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-             />
         </Head>
         <Home language={language}/>
     </>
