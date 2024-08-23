4730ea429394a3d70c2f182eadccd51e52115b76
Majid Askarifarsangi <Majid-2@celesteai.com>
2024-08-23 17:28:29 +0200

    Added meta tag rating

--- next.config.js
+++ next.config.js
@@ -4,7 +4,25 @@ const nextConfig = {
     plugins: [['styled-components', { ssr: true }]],
   },
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone', // Added missing comma
+  async headers() {
+    // Moved this to align properly in the object
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noarchive',
+          },
+          {
+            key: 'Content-Rating',
+            value: 'General',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
