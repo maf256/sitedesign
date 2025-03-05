90e56ed7e5f9ee33c2b787e44f9c8a44d1c55fc6
Majid Askarifarsangi <maf256@gmail.com>
2025-03-05 17:50:38 +0100

    Changed homepage context and styled

--- components/home/Home.js
+++ components/home/Home.js
@@ -16,14 +16,15 @@ export default function Home({ language, theme }) {
             priority={false}
           />
         </Card>
-        <div>
-          <H2>{Data.home[language ? 'en' : 'nr'].name}</H2>
-          <h3>
-            {Data.home[language ? 'en' : 'nr'].subject}{' '}
-            <b>{Data.about[language ? 'en' : 'nr'].h2}</b>.
-          </h3>
-          <h3>{Data.home[language ? 'en' : 'nr'].text2}</h3>
-          <SosialConteiner>
+        <Div>
+          <h1>{Data.home[language ? 'en' : 'nr'].h1}</h1>
+          <h2>{Data.home[language ? 'en' : 'nr'].h2}</h2>
+          <h3>{Data.home[language ? 'en' : 'nr'].h3}</h3>
+          <h4>{Data.home[language ? 'en' : 'nr'].h4}</h4>
+          <h5>{Data.home[language ? 'en' : 'nr'].h5}</h5>
+          <p>{Data.home[language ? 'en' : 'nr'].p}</p>
+          <p>{Data.home[language ? 'en' : 'nr'].p1}</p>
+          <SocialContainer>
             <a
               target="_blank"
               rel="noreferrer"
@@ -61,20 +62,8 @@ export default function Home({ language, theme }) {
               webutvikler
               <Svg.Email fontSize="45px">webutvikler</Svg.Email>
             </a>
-          </SosialConteiner>
-        </div>
-
-        <DetailContainer>
-          <p>{Data.about[language ? 'en' : 'nr'].txt1}</p>
-          <p>{Data.about[language ? 'en' : 'nr'].txt2}</p>
-          <p>{Data.about[language ? 'en' : 'nr'].txt3}</p>
-          <p>{Data.about[language ? 'en' : 'nr'].txt4}</p>
-          <p>
-            {Data.about[language ? 'en' : 'nr'].txt5}
-            <strong>webutvikler</strong>
-            {Data.about[language ? 'en' : 'nr'].txt6}
-          </p>
-        </DetailContainer>
+          </SocialContainer>
+        </Div>
       </Main>
     </>
   )
@@ -82,80 +71,199 @@ export default function Home({ language, theme }) {
 
 const Main = styled.main`
   background-color: ${({ theme }) => theme.BC_Home};
+  color: ${({ theme }) => theme.Text_Paragraph};
   min-height: 100vh;
   display: flex;
-  width: 100%;
-  justify-content: center;
   flex-direction: column;
   align-items: center;
+  padding: 40px;
+
   @media (min-width: 900px) {
     margin-left: 25%;
     width: 75%;
   }
+
   @media (max-width: 899px) {
     width: 100%;
-    flex-direction: column;
-    padding: 30px;
-  }
-  div {
-    h2 {
-      margin: 20px 0px;
-      font-size: 45px;
-      color: ${({ theme }) => theme.Text_Logo};
-    }
-    h3 {
-      margin: 10px 0px;
-      color: ${({ theme }) => theme.Text_Paragraph};
-      font-size: 18px;
-    }
+    padding: 20px;
   }
 `
 
-const H2 = styled.h2`
-  color: ${({ theme }) => theme.Text_Title};
+const Div = styled.div`
+  max-width: 800px;
+  text-align: center;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin-bottom: 15px;
+    line-height: 1.4;
+  }
+
+  h1 {
+    font-size: 2rem;
+    color: ${({ theme }) => theme.Text_Logo};
+  }
+
+  h2 {
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.Text_Logo};
+  }
+
+  h3,
+  h4,
+  h5 {
+    color: ${({ theme }) => theme.Text_Paragraph};
+    font-size: 1.1rem;
+  }
+
+  p {
+    font-size: 1.1rem;
+    line-height: 1.7;
+    margin-bottom: 20px;
+  }
 `
 
-const SosialConteiner = styled.span`
-  margin: 20px;
-  width: 300px;
+const SocialContainer = styled.div`
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 20px;
+
   a {
-    color: transparent;
-    font-size: 0px;
+    color: ${({ theme }) => theme.Icon_Color};
+    font-size: 0;
+
+    svg {
+      width: 35px;
+      height: 35px;
+    }
+
+    &:hover svg,
+    &:focus svg {
+      transform: scale(1.1);
+    }
   }
 `
+
 const Card = styled.div`
   img {
-    width: 550px;
+    width: 100%;
+    max-width: 450px;
     height: auto;
-    @media (max-width: 1300px) {
-      width: 350px;
-      height: auto;
+    border-radius: 8px;
+
+    @media (max-width: 900px) {
+      max-width: 350px;
     }
-    @media (max-width: 899px) {
-      width: 500px;
-      height: auto;
-    }
+
     @media (max-width: 500px) {
-      width: 400px;
-      height: auto;
+      max-width: 300px;
     }
   }
 `
 
-export const DetailContainer = styled.div`
-  border-top: solid 1px ${({ theme }) => theme.Text_Paragraph};
-  border-bottom: solid 1px ${({ theme }) => theme.Text_Paragraph};
-  margin: 0px 10px;
-  @media (max-width: 500px) {
-    margin: 0px 0px;
-  }
+const DetailContainer = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.Border_Color};
+  border-bottom: 1px solid ${({ theme }) => theme.Border_Color};
+  padding: 20px 0;
+
   p {
-    margin: 20px 0px;
-    color: ${({ theme }) => theme.Text_Paragraph};
-    line-height: 1.6;
-    font-size: 18px;
+    font-size: 1rem;
+    line-height: 1.7;
+  }
+
+  @media (max-width: 500px) {
+    padding: 15px 0;
   }
 `
+
+// const Main = styled.main`
+//   background-color: ${({ theme }) => theme.BC_Home};
+//   min-height: 100vh;
+//   display: flex;
+//   width: 100%;
+//   justify-content: center;
+//   flex-direction: column;
+//   align-items: center;
+//   @media (min-width: 900px) {
+//     margin-left: 25%;
+//     width: 75%;
+//   }
+//   @media (max-width: 899px) {
+//     width: 100%;
+//     flex-direction: column;
+//     padding: 30px;
+//   }
+// `
+// const Div = styled.div`
+//   h1 {
+//     margin: 20px 0px;
+//     font-size: 35px;
+//     color: ${({ theme }) => theme.Text_Logo};
+//   }
+//   h2 {
+//     margin: 20px 0px;
+//     font-size: 25px;
+//     color: ${({ theme }) => theme.Text_Logo};
+//   }
+//   h3 {
+//     margin: 10px 0px;
+//     color: ${({ theme }) => theme.Text_Paragraph};
+//     font-size: 18px;
+//   }
+//   h4,
+//   h5 {
+//     margin: 10px 0px;
+//     color: ${({ theme }) => theme.Text_Paragraph};
+//     font-size: 15px;
+//   }
+// `
+
+// const SosialConteiner = styled.span`
+//   margin: 20px;
+//   width: 300px;
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: center;
+//   a {
+//     color: transparent;
+//     font-size: 0px;
+//   }
+// `
+// const Card = styled.div`
+//   padding: 20px;
+//   img {
+//     width: 450px;
+//     height: auto;
+//     @media (max-width: 1300px) {
+//       width: 350px;
+//       height: auto;
+//     }
+//     @media (max-width: 899px) {
+//       width: 500px;
+//       height: auto;
+//     }
+//     @media (max-width: 500px) {
+//       width: 400px;
+//       height: auto;
+//     }
+//   }
+// `
+
+// export const DetailContainer = styled.div`
+//   border-top: solid 1px ${({ theme }) => theme.Text_Paragraph};
+//   border-bottom: solid 1px ${({ theme }) => theme.Text_Paragraph};
+//   margin: 0px 10px;
+//   @media (max-width: 500px) {
+//     margin: 0px 0px;
+//   }
+//   p {
+//     margin: 20px 0px;
+//     color: ${({ theme }) => theme.Text_Paragraph};
+//     line-height: 1.6;
+//     font-size: 18px;
+//   }
+// `
--- styles/theme.js
+++ styles/theme.js
@@ -1,18 +1,32 @@
+// // Ture is dark mode
 
-
-// Ture is dark mode
+// export default function theme(isDark) {
+//     return {
+//         Text_Title: isDark ?  '#bbbbb8' : '#787878',
+//         Text_Logo: isDark ?  '#fff' : '#000',
+//         Text_Menu: isDark ?  '#bbbbb4' : '#767676',
+//         Text_Menu_Hover: isDark ?  '#fff' : '#000',
+//         BC_Menu: isDark ?  '#494b50' : 'white',
+//         BC_Home: isDark ?  '#34353a' : '#f8f8f8',
+//         BC_Home2: isDark ?  '#494b50' : 'white',
+//         Icon_Color: isDark ? 'white' : 'black',
+//         Icon_bkColor: isDark ? '#606680' : '#F0F1F4',
+//         Text_Paragraph: isDark ? '#bbbbb8' : '#2E2A2A',
+//     }
+// }
 
 export default function theme(isDark) {
-    return {
-        Text_Title: isDark ?  '#bbbbb8' : '#787878',
-        Text_Logo: isDark ?  '#fff' : '#000',
-        Text_Menu: isDark ?  '#bbbbb4' : '#767676',
-        Text_Menu_Hover: isDark ?  '#fff' : '#000',
-        BC_Menu: isDark ?  '#494b50' : 'white',
-        BC_Home: isDark ?  '#34353a' : '#f8f8f8',
-        BC_Home2: isDark ?  '#494b50' : 'white',
-        Icon_Color: isDark ? 'white' : 'black',
-        Icon_bkColor: isDark ? '#606680' : '#F0F1F4',
-        Text_Paragraph: isDark ? '#bbbbb8' : '#2E2A2A',
-    }
+  return {
+    Text_Title: isDark ? '#CCCCCC' : '#595959',
+    Text_Logo: isDark ? '#FFFFFF' : '#000000',
+    Text_Menu: isDark ? '#DDDDDD' : '#333333',
+    Text_Paragraph: isDark ? '#D0D0D0' : '#505050',
+    Icon_Color: isDark ? '#FFFFFF' : '#000000',
+    Icon_bkColor: isDark ? '#606680' : '#F0F1F4',
+    BC_Home: isDark ? '#494B50' : '#F8F8F8',
+    BC_Home2: isDark ? '#606680' : '#FFFFFF',
+    Text_Title: isDark ? '#CCCCCC' : '#595959',
+    Text_Logo: isDark ? '#FFFFFF' : '#000000',
+    Text_Menu: isDark ? '#E0E0E0' : '#606060',
+  }
 }
