a405ff847728f8aaae90428530e95cf1ca492a23
Majid Askarifarsangi <maf256@gmail.com>
2025-03-17 18:45:50 +0100

    update next js and react

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  babel: {
-    plugins: [['styled-components', { ssr: true }]],
-  },
   reactStrictMode: true,
-  output: 'standalone', // Added missing comma
+  output: 'standalone',
+  compiler: {
+    styledComponents: true, // Optional but recommended for styled-components
+  },
   async headers() {
-    // Moved this to align properly in the object
     return [
       {
         source: '/(.*)',
         headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'noarchive',
-          },
-          {
-            key: 'Content-Rating',
-            value: 'General',
-          },
+          { key: 'X-Robots-Tag', value: 'noarchive' },
+          { key: 'Content-Rating', value: 'General' },
         ],
       },
     ]
