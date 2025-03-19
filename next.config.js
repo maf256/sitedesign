fe6ebf5df5986d9d1b170199d2ef0f21ef426ae2
Majid Askarifarsangi <maf256@gmail.com>
2025-03-19 19:25:37 +0100

    restory babelrc next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,47 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: 'standalone',
+//   compiler: {
+//     styledComponents: true, // Optional but recommended for styled-components
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//           { key: 'X-Robots-Tag', value: 'noarchive' },
+//           { key: 'Content-Rating', value: 'General' },
+//         ],
+//       },
+//     ]
+//   },
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  compiler: {
-    styledComponents: true, // Optional but recommended for styled-components
+  babel: {
+    plugins: [['styled-components', { ssr: true }]],
   },
+  reactStrictMode: true,
+  output: 'standalone', // Added missing comma
+
   async headers() {
+    // Moved this to align properly in the object
     return [
       {
         source: '/(.*)',
         headers: [
-          { key: 'X-Robots-Tag', value: 'noarchive' },
-          { key: 'Content-Rating', value: 'General' },
+          {
+            key: 'X-Robots-Tag',
+            value: 'noarchive',
+          },
+          {
+            key: 'Content-Rating',
+            value: 'General',
+          },
         ],
       },
     ]
